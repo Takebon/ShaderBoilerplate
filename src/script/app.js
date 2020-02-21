ec8e072f23a62b8bab5462a1252993867b0c7c07
TakebonCog <hrichard@hotmail.hu>
2020-02-21 10:21:47 +0100

    boilerplate

---
+++ src/script/app.js
@@ -0,0 +1,11 @@
+import '../css/style.scss'
+import vertexShader from '../shaders/vertexShader.glsl'
+import fragmentShader from '../shaders/fragmentShader.glsl'
+
+class App{
+  constructor(){
+
+  }
+}
+
+const app = new App()
